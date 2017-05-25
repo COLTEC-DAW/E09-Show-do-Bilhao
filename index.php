a8eb2371c089e4ada799194276b458051fdbfe62
Mateuhslc <matheuslcoelho@yahoo.com.br>
2017-05-24 22:51:56 -0300

    Index implementado com arquivos de texto

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Show do Bilhão</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1 style="text-align: center;color: #7cfc00;text-shadow: 2px 2px darkgreen">$how do Bilhão</h1>
+	<?php
+        $perg = fopen('perguntas.txt','r');
+		$perguntas = array();
+        while ($line = fgets($perg)) {
+            array_push($perguntas, $line);
+        }
+        fclose($perg);
+
+        $resp = fopen('respostas.txt','r');
+		$respostas = array();
+        $respostas_unica = array();
+        while ($line = fgets($resp)) {
+            $i++;
+            array_push($respostas_unica, $line);
+            if($i == 3){
+                $i = 0;
+                array_push($respostas, $respostas_unica);
+                $respostas_unica = array();
+            }
+        }
+        fclose($resp);
+
+        $gab = fopen('gabarito.txt', 'r');
+        $gabarito = array();
+        while ($line = fgets($gab)) {
+            array_push($gabarito, $gab);
+        }
+        fclose($gab);
+
+		foreach ($perguntas as $i => $info) {
+			echo "<h2>$i - $info<br></h2>";
+			foreach ($respostas[$i] as $j => $info2) {
+				echo "$j) $info2<br>";
+			}
+			echo "<br>";
+		}
+	?>
+</body>
+</html>
