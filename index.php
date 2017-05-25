2f555b359ee07a18c3bd49f9cd6b2e81208ec955
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2017-05-25 11:10:09 -0300

    Inicio da Atividade

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Show do Bilhão</title>
+		<meta charset="utf-8">
+	</head>
+
+	<body>
+		<?php include("menu.inc"); ?>
+
+
+
+		<?php include("rodape.inc"); ?>
+	</body>
+</html>
