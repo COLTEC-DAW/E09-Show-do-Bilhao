b1482e71d7b2270c12701f1a77f1636f09687ead
Lucas <lucascoltec2016@gmail.com>
2017-05-25 11:10:39 -0300

    Tá meio bugado

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
 	<meta charset="utf-8">
 </head>
 <body>
+	<?php require("functions.inc"); ?>
 	<h1 style="text-align: center;color: #7cfc00;text-shadow: 2px 2px darkgreen">$how do Bilhão</h1>
 	<?php
 		$perguntas = array("1 + 1 = ?", "Qual a fruta que se seca para fazer a ameixa seca?", "De qual grupo de samba fez parte o humorista \"Mussum\"?", "Qual desses símbolos significa quilômetro?","Qual é o animal que representa o signo de touro?","Como é chamada a doença que clareou a pele do Michael Jackson?");
-		$respostas = array(
+		$alternativas = array(
 			array("1","2","0","3"),
 			array("Ameixa","Maçã","Laranja","Beterraba"),
 			array("Fundo de Quintal","Titulares do Ritmo","Originais do Samba","Banda Brasil"),
@@ -16,11 +17,12 @@
 			array("Touro","Cavalo","Hipopótamo","Galo"),
 			array("Astigmatismo","Pedofilia","Vitiligo","Bruxismo")
 		);
-		foreach ($perguntas as $index1 => $info1) {
-			$index1++;
-			echo "<h3>$index1 - $info1<br></h3>";
+		$respostas = array(2, 1, 3, 2, 3);
+		carregaPergunta(1);
+		/*foreach ($perguntas as $index1 => $info1) {
+			echo "<h3>" . ++$index1 . " - $info1<br></h3>";
 			$index1--;
-			foreach ($respostas[$index1] as $index2 => $info2) {
+			foreach ($alternativas[$index1] as $index2 => $info2) {
 				switch ($index2) {
 					case '0': echo "a)";
 						break;
@@ -34,7 +36,7 @@
 				echo " $info2<br>";
 			}
 			echo "<br>";
-		}
+		}*/
 	?>
 </body>
 </html>
