f1dc54e0bc5c297e19ff30eff33530e82cdb76a8
aluno <aluno@ly784.local>
2017-05-25 10:54:07 -0300

    Comecei a fazer

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Show do Milhão</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+    <body>
+        <div class="titulo">
+            <h1>Show do Milhão</h1>
+            <div class="subtitulo">
+                <p>Responda ao máximo de perguntas que conseguir!</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <?php
+                echo "Olá, Terráqueo. Agora são: ";
+            ?>
+        </div>
+    </body>
+</html>
