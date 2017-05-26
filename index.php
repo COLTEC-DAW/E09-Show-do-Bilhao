90e1b858efe37dda6e1fa7adfec00536085bb30c
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2017-05-26 14:49:43 -0300

    Exercicio Finalizado!

--- index.php
+++ index.php
@@ -7,8 +7,22 @@
 
 	<body>
 		<?php include("menu.inc"); ?>
+		<?php require 'perguntas.inc'; ?>
 
+		<?php
+			$id = $_GET["id"];
+			$dados = carregaPergunta($id);
 
+			//Exibicao
+			echo $id + 1 . ") " . $dados[0];
+			echo "<ul>";
+
+			for ($j = 0; $j < 5; $j++) { 
+				echo "<li>" . $dados[1][$j] . "</li>";
+			}
+
+			echo "</ul>";
+		?>
 
 		<?php include("rodape.inc"); ?>
 	</body>
