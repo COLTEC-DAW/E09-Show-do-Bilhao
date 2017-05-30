57cb8c2ea3f89a3742a23eb7b94a852ca7c69e62
VictorHSMoura <victorhugosmoura@gmail.com>
2017-05-30 15:34:56 -0300

    Atividade completa

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang>
+<html>
     <head>
         <title></title>
         <meta charset="UTF-8">
@@ -9,25 +9,26 @@
     <body>
         <h1>Bem vindo ao $how do Bilhão</h1>
         <?php
-            $perguntas = array("Como se chama o lago salgado, que é parte mais baixa da Terra, situado na Palestina?", "Qual o continente que tem mais países?", "A planície da Manchúria encontra-se em qual país?", "Na fronteira de quais países está o Everest, pico mais alto do mundo?", "O abacaxi é originário de que país?");
+            require 'dados.inc';
+
+            global $perguntas, $alternativas, $respostas, $alt;
+            $perguntas = array( "Como se chama o lago salgado, que é parte mais baixa da Terra, situado na Palestina?",
+                                "Qual o continente que tem mais países?",
+                                "A planície da Manchúria encontra-se em qual país?",
+                                "Na fronteira de quais países está o Everest, pico mais alto do mundo?",
+                                "O abacaxi é originário de que país?");
             $alternativas = array(
-                                array("Mar Morto", "Mar Cáspio", "Mar Mediterrâneo", "Mar do Caribe"),
-                                array("Europa", "África", "Ásia", "América"),
-                                array("China", "Mongólia", "Irã", "Angola"),
-                                array("Suíça e Itália", "Nepal e China", "Chile e Argentina", "Brasil e Uruguai"),
-                                array("Estados Unidos", "Brasil", "Colômbia", "Venezuela")
-                            );
+                array("Mar Morto", "Mar Cáspio", "Mar Mediterrâneo", "Mar do Caribe"),
+                array("Europa", "África", "Ásia", "América"),
+                array("China", "Mongólia", "Irã", "Angola"),
+                array("Suíça e Itália", "Nepal e China", "Chile e Argentina", "Brasil e Uruguai"),
+                array("Estados Unidos", "Brasil", "Colômbia", "Venezuela")
+            );
             $respostas = array(0, 1, 0, 1, 1);
             $alt = array("a)", "b)", "c)", "d)");
-            for($i = 0; $i < 5; $i++){
-                echo $i+1;
-                echo ") $perguntas[$i]<br>";
-                $alter = $alternativas[$i];
-                for($j = 0; $j < 4; $j++){
-                        echo "$alt[$j] $alter[$j]<br>";
-                }
-                echo "<br>";
-            }
+
+            $id_pergunta = $_GET["id"];
+            pergunta($id_pergunta);
         ?>
     </body>
 </html>
