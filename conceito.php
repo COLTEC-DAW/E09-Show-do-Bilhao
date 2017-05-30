2b7aaf3e1a80fa5073118d121b7f88429712af30
francisco fabiano neves <a2015951614@lycda.local>
2017-05-30 15:16:30 -0300

    Página conceito renomeada para organizar melhor

---
+++ conceito.php
@@ -0,0 +1,177 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>$HOW DO BIÃO</title>
+    </head>
+    <body>
+        <h1>Bem vindo à prova conceito do Show do Bilhão</h1>
+        <?php
+            $perguntas = [];
+            $respostas = [];
+            $corretas = [];
+            for ($i=0; $i<20; $i++) $respostas[$i]=[];
+
+            $perguntas[0] = "Em qual país foi invetado o brinquedo LEGO?";
+            $respostas[0][0] = "Estados Unidos";
+            $respostas[0][1] = "Japão";
+            $respostas[0][2] = "Itália";
+            $respostas[0][3] = "Dinamarca";
+            $corretas[0] = 3;
+
+            $perguntas[1] = "Qual desses astros ainda está vivo (24/05/2017)?";
+            $respostas[1][0] = "Elvis Presley";
+            $respostas[1][1] = "Paul McCartney";
+            $respostas[1][2] = "Michael Jackson";
+            $respostas[1][3] = "Prince";
+            $corretas[1] = 1;
+
+            $perguntas[2] = "Onde fica o Oceano Índico?";
+            $respostas[2][0] = "No Pólo Norte";
+            $respostas[2][1] = "Perto da Índia";
+            $respostas[2][2] = "Entre a Europa e a África";
+            $respostas[2][3] = "Ao redor da Austrália";
+            $corretas[2] = 2;
+
+            $perguntas[3] = "VHDL é uma linguagem de:";
+            $respostas[3][0] = "Descrição";
+            $respostas[3][1] = "Programação";
+            $respostas[3][2] = "Tortura";
+            $respostas[3][3] = "Nenhuma das anteriores";
+            $corretas[3] = 2;
+
+            $perguntas[4] = "Por qual motivo você deve sempre identar seu código?";
+            $respostas[4][0] = "Os veteranos vão te matar se não fizer isso";
+            $respostas[4][1] = "Vale Ponto";
+            $respostas[4][2] = "As curvas ficam sexy";
+            $respostas[4][3] = "Todas as anteriores";
+            $corretas[4] = 3;
+
+            $perguntas[5] = "Qual dos seguinte mitos sobre o Honda é verdade?";
+            $respostas[5][0] = "Ele mora sozinho no meio do nada";
+            $respostas[5][1] = "Ele teleporta";
+            $respostas[5][2] = "Ele mora com ninguém sabe quantos caras";
+            $respostas[5][3] = "Todas as anteriores";
+            $corretas[5] = 3;
+
+            $perguntas[6] = "Quando improvisando um quiz para um exercício, você usa skillz de:";
+            $respostas[6][0] = "Infação";
+            $respostas[6][1] = "Migué";
+            $respostas[6][2] = "Criatividade";
+            $respostas[6][3] = "McGyverismo";
+            $corretas[6] = 2;
+
+            $perguntas[7] = "Se a resposta dessa pergunta fosse a segunda bolinha, qual você marcaria?";
+            $respostas[7][0] = "A primeira";
+            $respostas[7][1] = "A terceira";
+            $respostas[7][2] = "A segunda";
+            $respostas[7][3] = "A últimaa";
+            $corretas[7] = 2;
+
+            $perguntas[8] = "Qual desses jogos é infame por ser difícil?";
+            $respostas[8][0] = "Super Mario World para SNES";
+            $respostas[8][1] = "O show do Milhão";
+            $respostas[8][2] = "Dark Souls para PS3, PC e XB360";
+            $respostas[8][3] = "The Legend of Zelda: Ocarina of Time para o N64";
+            $corretas[8] = 2;
+
+            $perguntas[9] = "8) Quantas perguntas já foram feitas?";
+            $respostas[9][0] = "10";
+            $respostas[9][1] = "9";
+            $respostas[9][2] = "8";
+            $respostas[9][3] = "7";
+            $corretas[9] = 0;
+
+            $perguntas[10] = "Em qual século Gandhi nasceu?";
+            $respostas[10][0] = "XIX";
+            $respostas[10][1] = "XV";
+            $respostas[10][2] = "XII";
+            $respostas[10][3] = "Nenhum. Ele é um personagem mítico";
+            $corretas[10] = 0;
+
+            $perguntas[11] = "Onde a pizza foi inventada?";
+            $respostas[11][0] = "Itália";
+            $respostas[11][1] = "Grécia";
+            $respostas[11][2] = "Estados Unidos";
+            $respostas[11][3] = "Austrália";
+            $corretas[11] = 1;
+
+            $perguntas[12] = "Em qual continente fica a Áustria?";
+            $respostas[12][0] = "África";
+            $respostas[12][1] = "Oceania";
+            $respostas[12][2] = "América Central";
+            $respostas[12][3] = "Europa";
+            $corretas[12] = 3;
+
+            $perguntas[13] = "O Mediterrâneo é...";
+            $respostas[13][0] = "Uma cordilheira";
+            $respostas[13][1] = "Um mar";
+            $respostas[13][2] = "Um lago";
+            $respostas[13][3] = "Uma montanha";
+            $corretas[13] = 1;
+
+            $perguntas[14] = "Qual das seguintes músicas NÃO é dos Beatles?";
+            $respostas[14][0] = "Cry, Baby, Cry";
+            $respostas[14][1] = "Start me Up";
+            $respostas[14][2] = "Ob-la-di, Ob-la-da";
+            $respostas[14][3] = "Yellow Submarine";
+            $corretas[14] = 1;
+
+            $perguntas[15] = "Qual das seguintes bandas NÃO é inglesa?";
+            $respostas[15][0] = "The Rolling Stones";
+            $respostas[15][1] = "The Beatles";
+            $respostas[15][2] = "Led Zeppelin";
+            $respostas[15][3] = "The White Stripes";
+            $corretas[15] = 3;
+
+            $perguntas[16] = "O filme Shrek é de qual estúdio de animação?";
+            $respostas[16][0] = "DreamWorks";
+            $respostas[16][1] = "Disney";
+            $respostas[16][2] = "Sony";
+            $respostas[16][3] = "Pixar";
+            $corretas[16] = 0;
+
+            $perguntas[17] = "Qual é o nome do companheiro de Sherlock Holmes?";
+            $respostas[17][0] = "Moriarty";
+            $respostas[17][1] = "Poirot";
+            $respostas[17][2] = "Watson";
+            $respostas[17][3] = "Arsene Lupin";
+            $corretas[17] = 2;
+
+            $perguntas[18] = "Mas o mago é...";
+            $respostas[18][0] = "Poderoso";
+            $respostas[18][1] = "Fedido";
+            $respostas[18][2] = "Dumbledore";
+            $respostas[18][3] = "Implacável";
+            $corretas[18] = 3;
+
+            $perguntas[19] = "QUEM é \"O Senhor dos Anéis\" ao qual o título dos filmes e livros se refere?";
+            $respostas[19][0] = "Frodo";
+            $respostas[19][1] = "Sauron";
+            $respostas[19][2] = "Gandalf";
+            $respostas[19][3] = "Gollum";
+            $corretas[19] = 1;
+
+            for($i = 0; $i<20; $i++){
+                echo $perguntas[$i];
+                echo nl2br("\n\n");
+            ?>
+            <FORM name="resposta" method="post" action="respostas.php">
+                <Input type = 'Radio' name = "<?php echo $i ?>" value=0>
+                    <?php echo $respostas[$i][0] ?>
+                    <br>
+                <Input type = 'Radio' name = "<?php echo $i ?>" value=1>
+                    <?php echo $respostas[$i][1] ?>
+                    <br>
+                <Input type = 'Radio' name = "<?php echo $i ?>" value=2>
+                    <?php echo $respostas[$i][2] ?>
+                    <br>
+                <Input type = 'Radio' name = "<?php echo $i ?>" value=3>
+                    <?php echo $respostas[$i][3] ?>
+                    <br>
+            </FORM>
+            <?php
+                echo nl2br("\n");
+            }
+        ?>
+    </body>
+</html>
