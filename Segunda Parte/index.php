6916636ac63a87ef9361a20745c169142c749d5c
anandateodoro <teodoroananda@gmail.com>
2017-05-30 06:23:29 -0300

    Segunda parte ok

---
+++ Segunda Parte/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Jogo do Bilhão</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+   <body>
+          <h1 id="tit"> 
+            O melhor jogo do bilhão 
+        </h1>
+         <p> Clique no Silvio para começar </p>
+         <br>
+       <a href="perguntas.php?id=0"> <img src="silvio.jpg" height="500" width="450"> </a>
+
+
+   </body>
+</html>
