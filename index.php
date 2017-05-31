108b0a6fe83a70427df50740bb95bf6bc0ae3937
Gustavo de Paula <gustavo@depaula.cz>
2017-05-30 23:52:23 -0300

    Dá Bilhão? Pt2.1 Design

--- index.php
+++ index.php
@@ -3,11 +3,32 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Dá Bilhão?</title>
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+	<link rel="stylesheet" href="css/main.css">
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-	<a class="" href="perguntas.php?id=1">Começar</a>
-<?php
+	<?php include 'menu.inc'; ?>
+	
+	<main>
+		<div class="container">
+			<h1 class="center-align">Vamos começar?</h1>
+			<div class="row">
+				<a class="waves-effect waves-light btn-large center-align col s12 green accent-4" href="perguntas.php?id=1">Bora.</a>
+			</div>
+		</div>
+		
+	</main>
 
-?>
+
+	<?php  include 'rodape.inc';	?>
+
+	<!--Import jQuery before materialize.js-->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
 </body>
 </html>
