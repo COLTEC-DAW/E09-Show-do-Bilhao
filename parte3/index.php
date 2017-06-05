4d5c08d21fd2270f7f344e7eb797baf149f6b4e6
raul4247 <raulfmansur@hotmail.com>
2017-06-05 20:12:34 -0300

    Parte 3 finalizada

---
+++ parte3/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Show do Bilhão</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+	<link rel="stylesheet" href="/css/index-style.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="jumbotron">
+					<h2>
+						Show do Bilhão!
+					</h2>
+					<p>
+						Clique no botão para iniciar.
+					</p>
+				
+					<img src="http://imgsapp.diariodepernambuco.com.br/app/noticia_127983242361/2016/05/10/643641/20160510103231460315e.jpeg"/>
+					</br>
+		
+					<form action='perguntas.php' method='POST'>
+						<input type='hidden' name='id' value='0'>
+						<input type='submit' value='submit'/>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
