add8130d597ee0d259952cf05fe3675a78dcb9d7
Lucas <lucaspaulom@hotmail.com>
2017-06-05 14:32:42 -0300

    Parte 3.

---
+++ Parte 3/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+	<title>Show do Bilhão</title>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="jumbotron">
+					<h1>Show do Bilhão</h1>
+					<p>O Show do Bilhão é um programa idealizado pela emissora SBT (Sistema Belo-Horizontino de Televisão). Neste programa, um candidato escolhido da audiência é submetido a uma sequência de 5 perguntas de conhecimento geral. A medida em que o candidato responde cada pergunta ele avança no jogo.</p>
+					<p>O jogo termina quando o candidato responde uma pergunta incorretamente. Após o término do jogo o sistema calcula a pontuação final do candidato. Sua pontuação é dada pela quantidade de perguntas respondidas corretamente pelo candidato.</p>
+					<p class="final">Vamos jogar?</p>
+					<div class="botao">
+						<a class="btn btn-primary btn-large" href="perguntas.php?id=0">Iniciar</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ Parte 3/resultados.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+	<title>Show do Bilhão</title>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+
+			<div class="col-md-12 jumbotron">
+				<?php include 'menu.inc';?>
+			</div>
+
+			<div class="col-md-12">
+				<?php
+					$pontos = $_COOKIE['pontuacao']; 
+					echo "<p>Você fez ".$pontos." pontos em 5 questões</p>";
+				?>
+			</div>
+
+			<div id="footer" class="col-md-12">
+				<?php include 'rodape.inc';?>
+			</div>
+
+		</div>
+	</div>
+
+</body>
+</html>
