43334e688940722a50074a423171a38263dd9d90
Lucas <lucascoltec2016@gmail.com>
2017-06-06 15:29:34 -0300

    Correção in progress

--- checaResposta.php
+++ checaResposta.php
@@ -1,3 +1,11 @@
 <?php
 	
+	require "dados.inc";
+
+	$novaPergunta = $_POST["pergunta+1"];
+
+	if($_POST["resposta"] == $respostas[$_POST["pergunta"]]){
+		header("location: pergunta.php?id=$novaPergunta");
+	}
+	header("location: gameover.html");
 ?>
--- pergunta.php
+++ pergunta.php
@@ -8,21 +8,7 @@
 	<?php
 		include 'menu.inc';
 		require 'functions.inc';
-
-		global $perguntas;
-		$perguntas = array("1 + 1 = ?", "Qual a fruta que se seca para fazer a ameixa seca?", "De qual grupo de samba fez parte o humorista \"Mussum\"?", "Qual desses símbolos significa quilômetro?","Qual é o animal que representa o signo de touro?","Como é chamada a doença que clareou a pele do Michael Jackson?");
-
-		global $alternativas;
-		$alternativas = array(
-			array("1","2","0","3"),
-			array("Ameixa","Maçã","Laranja","Melão"),
-			array("Fundo de Quintal","Titulares do Ritmo","Originais do Samba","Banda Brasil"),
-			array("KD","KM","KK","KG"),
-			array("Touro","Cavalo","Hipopótamo","Galo"),
-			array("Astigmatismo","Pedofilia","Vitiligo","Bruxismo")
-		);
-
-		$respostas = array(2, 1, 3, 2, 3);
+		require 'dados.inc';
 
 		$numPergunta = $_GET["id"];
 		carregaPergunta($numPergunta);
