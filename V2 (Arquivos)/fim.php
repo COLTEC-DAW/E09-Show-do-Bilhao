1a234a941ee142425d7f20632ac943ccd7fa9105
francisco fabiano neves <a2015951614@lycda.local>
2017-06-20 15:50:32 -0300

    V2 também funciona (date)

--- V2 (Arquivos)/fim.php
+++ V2 (Arquivos)/fim.php
@@ -1,20 +1,26 @@
+<? ob_start();
+session_start();?>
 <!Doctype>
 <html>
     <body>
         <?php include "menu.inc"; ?>
         <h1>RESULTADOS</h1>
         <?php
-        if ($_POST["win"]) echo "<h2>PARABÉNS, VOCÊ GANHOU!!!</h2>";
+        if ($_POST["pontos"]==20) echo "<h2>PARABÉNS, VOCÊ GANHOU!!!</h2>";
         else echo "<h2>Parece que não foi hoje...</h2>";
         ?>
         <p>Número de acertos: <?= $_POST["pontos"] ?></p>
         <?php
+        date_default_timezone_set("America/Sao_Paulo");
         echo "Sua última pontuação: ".$_COOKIE["lastScore"];
-        $_COOKIE["lastScore"] = $_POST["pontos"];
-        echo "Sua última partida: ".date('Y-m-d', $_COOKIE["lastDate"]); 
-        $_COOKIE["lastDate"] = time();
+        setcookie("lastScore", $_POST["pontos"]);
+        echo "Sua última partida: ". date ("d/m/Y", $_COOKIE["lastDate"]); 
+        setcookie("lastDate", date("d/m/Y h:i:s"));
         ?>
-        <a href="logout.php">Sair</a>
+        <br>
+        <form method="get" action="logout.php">
+            <button type="submit">Sair</button>
+        </form>
         <?php include "rodape.inc"; ?>
     </body>
 </html>
