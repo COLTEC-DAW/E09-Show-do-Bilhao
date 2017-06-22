597ad2a5a8b944c39acb82a33828d27ad388936a
Lucas Macedo Bruzzi <lucascoltec2016@gmail.com>
2017-06-22 13:56:34 -0300

    Anti-cheat funcionando!

---
+++ anticheat.php
@@ -0,0 +1,4 @@
+<h1>QUANDO VOCÊ TENTAR BURLAR O JOGO, EU VOU ESTAR LÁ</h1>
+
+<form action="index.php">
+<input type="submit" value="Log in">
--- gameover.php
+++ gameover.php
@@ -21,11 +21,6 @@
 
 <?php
 	} else{
-?>
-	<h1>QUANDO VOCÊ TENTAR BURLAR O JOGO, EU VOU ESTAR LÁ</h1>
-
-	<form action="index.php">
-	<input type="submit" value="Log in">
-<?php
+		header("location: anticheat.php");
 	}
 ?>
