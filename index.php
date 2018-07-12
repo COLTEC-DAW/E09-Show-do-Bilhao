61d3749be15135360781e96355818aabd4830c83
ibryans <bryans0701@gmail.com>
2018-07-12 10:42:12 -0300

    Separando perguntas por página

--- index.php
+++ index.php
@@ -1,83 +1,44 @@
-<?php
-    $enunciados = [
-        "Quem traiu a cantora brasileira Joelma?",
-        "Na frase 'sabe de nada' do grande pensador contemporâneo Compadre Washington, o sujeito é:",
-        "Quem vai ganhar a Copa do Mundo 2018?",
-        "Um carro foda X Celta 2012, os dois a 80km, um fica do lado do outro?",
-        "Quem é o melhor rapper da atualidade?"
-    ];
-
-    $alternativas = [
-        $enunciados[0] => [
-            "A lua",
-            "O chimbinha",
-            "A lua e o chimbinha",
-            "Willian Bonner"
-        ],
-        $enunciados[1] => [
-            "Simples",
-            "Indeterminado",
-            "Oculto",
-            "Inocente"
-        ],
-        $enunciados[2] => [
-            "França",
-            "Brasil",
-            "Bélgica",
-            "Fagner"
-        ],
-        $enunciados[3] => [
-            "Creio que não",
-            "Claro",
-            "Sim, 80km é 80km",
-            "Menino Ney"
-        ],
-        $enunciados[4] => [
-            "Drake (humildão)",
-            "Eminem",
-            "xis xis xis tentação deus do rap (RIP)",
-            "Post Malone"
-        ]
-    ];
-
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/bulma.min.css">
         <title>Show do Bilhão - 2018</title>
     </head>
 
     <body>
         
-        <header>
-
-            <img src="images/show.jpg">
-            
-        </header>
+        <!-- Menu -->
+        <?php include "includes/topbar.inc"; ?>
 
         <div class="container">
 
-            <?php
+            <form action="index.php" method="GET">
 
-                foreach($enunciados as $enunciado) {
-                    echo "<h3 class='enunciado'> $enunciado </h3>";
-                    foreach($alternativas[$enunciado] as $alternativa) {
-                        echo "<div class='cont'>
-                                <input type='radio' name='$enunciado' class=''>
-                                    <span class='checkmark'>" . 
-                                    $alternativa . 
-                                    "</span>
-                              </div>";
+                <?php require 'includes/perguntas.inc';
+                    if ($_GET["id"] == null) {
+                        echo "<h1 class='title'> Bem vindo ao Show do Bilhão </h1>";
+                        echo "<p class='subtitle'> Responda a várias perguntas e concorra a 1 Bilhão de Bitcoins! (ou não) </p>";
+                        $tag = '<a class="button is-success is-pulled-left" href="index.php?';
+                        echo $tag . str_replace("0",$_GET['id']+0,'id=0"') . '>Começar</a>' . '</br>';
+                    } else {
+                        if($_GET["id"] < 5) {
+                            carregaPergunta($_GET["id"]);
+                            $tag = '<a class="button is-success is-pulled-right" href="index.php?';
+                            echo $tag . str_replace("0",$_GET['id']+1,'id=0"') . '>Continuar</a>' . '</br>';
+                        } else
+                            echo '<p>Jogo Terminado.</p>';
                     }
-                }
+                ?>
 
-            ?>
+            </form>
 
         </div>
+
+        <!-- Rodapé -->
+        <?php include "includes/rodape.inc"; ?>
 
     </body>
 </html>
---
+++ prova.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/bulma.min.css">
+        <title>Show do Bilhão - Prova de conceito</title>
+    </head>
+
+    <body>
+
+        <!-- Menu -->
+        <?php include "includes/topbar.inc"; ?>
+
+        <div class="content">
+
+            <div class="container">
+
+            <?php require "includes/perguntas.inc";
+
+                for ($i = 0; $i < 5; $i++) {
+                    carregaPergunta($i);
+                }
+
+            ?>
+
+            </div>
+
+        </div>
+
+        <!-- Rodapé -->
+        <?php include "includes/rodape.inc"; ?>
+
+    </body>
+</html>
