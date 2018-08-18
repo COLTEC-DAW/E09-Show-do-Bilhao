8ec1ed171f7a24cf9649cec784871d0a24f32b64
Dener Vieira <deneribeiro10@hotmail.com>
2018-08-18 19:30:11 -0300

    perfil

--- pages/perfil.php
+++ pages/perfil.php
@@ -16,9 +16,15 @@
             include '../partials/nav.inc'
         ?>
 
-        <div class="container mt-3 text-center">
-            <h1>Sua pontuação máxima</h1>
-            <h2><?= $perfil['placar'] ?></h2>
+        <div class="container text-center mt-3">
+            <p class="display-4">Olá <?= $perfil['nome'] ?>, está se divertindo?</p>
+            <h1 class="mt-3">Sua pontuação máxima</h1>
+            <h3 class=""><?= $perfil['placar'] ?></h3>
+            <h1 class="mt-3">Seu email</h1>
+            <h3 class=""><?= $perfil['email'] ?></h3>
+            <h1 class="mt-3">Seu login</h1>
+            <h3 class=""><?= $perfil['usuario'] ?></h3>
+            <h1 class="mt-3">Não se preocupe, não mostraremos sua senha</h1>
         </div>
 
         <?php include '../partials/footer.inc' ?>
