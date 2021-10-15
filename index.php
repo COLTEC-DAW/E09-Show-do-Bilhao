ae01d70f6d6695d8199eed7d9c68cab6a46fe92c
AvaMelillo <avamelillo@gmail.com>
2021-10-14 22:06:38 -0300

    Deixando o código mais legível

--- index.php
+++ index.php
@@ -7,22 +7,42 @@
     <head>
         <title>Jogo do Bilhão</title>
     </head>
+
     <body>
-        <form action="" method="post">
+        <form method="post">
             
             <?php
+
+                /** 
+                 * >>>  BEM VINDE AO CÓDIGO DO JOGO DO BILHÃO MAIS LINDO INCRIVEL MARAVILHOSO DE TODOS LOS TIEMPOS ABRAM ALAS  <<<
+                 * 
+                 * eu tentei deixar o código o mais legível possível, então pode contar com os comentários!
+                 * Alguns detalhes sobre o código:
+                 * 
+                 *  > TODAS AS CHAVES DE PROPRIEDADES ENVIADAS EM POST (EM TODO O PROGRAMA):
+                 * 
+                 *      >> login, nome, senha, email (pro cadastro/Login)
+                 *      >> cadastro (É enviada quando o usuário escolhe criar novo usuário)
+                 *      >> acao (ação escolhida pelo usuário após perder (voltar pro início ou fazer log out))
+                 *      >> indexPerguntaAtual (acho q é autoexplicativa neh kkk)
+                 *      >> alternativa (resposta escolhida pelo usuário naquela pergunta)
+                 *  
+                 *  > O QUE FALTA NO CÓDIGO/O QUE PRECISO MUDAR:
+                 * 
+                 *      >> Cookies (ultima data e pontuação)
+                 *      >> Uso de objetos (POO)
+                 *      >> Aparência melhor pra página de game over e de ganhou
+                 * 
+                */
 
                 // quando o jogador perde, ele decide se vai jogar novamente ou fazer logout e a opção escolhida
                 // é enviada com o nome "ação".
 
-                if  ( isset($_POST['acao'])) {
+                if  ( issEt($_POST['acao'])) {
 
                     //se a ação escolhida for o logout, a sessão é destruída:
 
-                    if ($_POST['acao'] == 'logout'){
-
-                        session_destroy();
-                    }
+                    if ($_POST['acao'] == 'logout') session_destroy();
                 }
 
 
@@ -34,59 +54,69 @@
                 else{
                     
                     //checa pra ver se o nome do usuário já foi inserido:
-
                     if  ( isset($_POST['nome'])) {
 
+                        
                         require "./lib/login.inc";
 
+                        //se sim, ele pega as informações inseridas e coloca em um array associativo:
                         $loginAtual = array("login" => $_POST['login'], "nome" => $_POST['nome'], "senha" => $_POST['senha'], "email" => $_POST['email']); 
+                        
+                        //...e o banco de dados com os usuários é importado.
                         $dadosUsuarios = json_decode(file_get_contents('./dados/usuarios.json'), true);
 
+                        //se o usuário tiver escolhido criar um novo cadastro...
                         if (isset($_POST['cadastro'])){
 
+                            //as informações inseridas são adicionadas ao banco de dados
                             $dadosUsuarios[] = $loginAtual;
                             file_put_contents('./dados/usuarios.json', json_encode($dadosUsuarios));
+                            //e ele é colocado na sessão.
                             $_SESSION['usuario'] = $_POST['nome'];
+
                         }
+                        // se o usuário já existir no banco de dados...
                         else if (autenticaLogin($dadosUsuarios, $loginAtual) == true){
 
+                            //a sessão é iniciada com ele.
                             $_SESSION['usuario'] = $_POST['nome'];
 
                         }
+                        //e se não existir...
                         else{
 
+                            //é enviado via POST um novo parâmetro que indica que o usuário ainda vai ser cadastrado:
                             echo "<p> Usuário não encontrado! Faça seu cadastro: </p>
                                     <input type=\"hidden\" formmethod=\"post\" name=\"cadastro\" value=\"true\">";
 
                         }
                     }
                 
-                    //e se ele já está na sessão.
+                    //Checa se a tem um usuário na sessão:
                     if (empty($_SESSION['usuario'])) {
     
                         //se não estiver, o formulário para fazer o login aparece:
-
                         require "./views/login.inc";
                         
                     }
                     else{
     
-            
-                        require "./lib/perguntas.inc";
+                        //
+                        //se já houver um usuário na sessão, o jogo começa.
+                        //
 
-                        $usuario = $_SESSION['usuario'];
-                        
+                        require "./lib/perguntas.inc";                        
             
                         // Pega os valores da pergunta anterior e a alternativa selecionada anteriormente
                         // ( ifs são para não dar warning na primeira pergunta)
-                        if ( isset($_POST['indexPerguntaAtual'])) {  $numPerg = (int)$_POST['indexPerguntaAtual'];}
-                        if ( isset($_POST['alternativa'])) {  $altMarcada = (int)$_POST['alternativa'];}
+                        if ( isset($_POST['indexPerguntaAtual']))  $numPerg = (int)$_POST['indexPerguntaAtual'];
+                        if ( isset($_POST['alternativa']))  $altMarcada = (int)$_POST['alternativa'];
     
-    
+
                         // Checa se as variáveis estão indefinidas, e as preenche com 0 (para não dar warning na primeira pergunta)
-                        if (  !isset($numPerg)  ) {  $numPerg = 0; }
-                        if ( !isset($altMarcada)) {  $altMarcada = 0; }
-    
+                        if (  !isset($numPerg)  ) $numPerg = 0;
+                        if ( !isset($altMarcada)) $altMarcada = 0;
+
 
                         // pega o arquivo com as informações das perguntas:
                         $dadosPerguntas = json_decode(file_get_contents('./dados/perguntas.json'), true);
@@ -95,11 +125,17 @@
                         // Confere se a opção anterior estava correta
                         if (autenticaOpcao($altMarcada, $numPerg, $dadosPerguntas) == true ){
                             
-                            echo "<p> Olá, $usuario</p>";
-                            carregaProgresso($numPerg);
-                            // e, se sim, carrega a próxima pergunta
-                            carregaPergunta($numPerg, $dadosPerguntas[$numPerg]);
-    
+                            if ($numPerg < 10){
+                                
+                                echo "<p> Olá, {$_SESSION['usuario']}</p>";
+                                carregaProgresso($numPerg);
+                                // e, se sim, carrega a próxima pergunta
+                                carregaPergunta($numPerg, $dadosPerguntas[$numPerg]);
+                            }
+                            else{
+
+                                require  "./views/ganhou.inc";
+                            }
                         }
                         else {
                             
