7fcfb552043d1b5d5ce4183d52c509a81e8df9e0
pedroping <2019951899@teiacoltec.org>
2021-10-20 18:34:26 -0300

    Separando uma pergunta por página

--- perguntas.php
+++ perguntas.php
@@ -1,31 +1,25 @@
-<?php
-    require "./perguntas.inc";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Show do Bilhao!!</title>
 </head>
 <body>
     <?php
         include "./menu.inc";
     ?>
     <div>
-        <h1>Show do Bilhao!!</h1>
-        <p>
+        <?php
+            require "./perguntas.inc";
             
-            <?php
-                for($i = 0;$i<count($GLOBALS["perguntas"]);$i++){
-                    echo "\nPergunta " . ($i + 1) . "\n\n";
-                    $pergunta_ = carregaPergunta($i);
-                    mostra_pergunta($pergunta_);
-                }
-            ?>
-        </p>
+            if(empty($_GET)){
+                mostra_pergunta(0);
+            }else{
+                mostra_pergunta($_GET['id']);
+            }
+           
+        ?>
     </div>
     <?php
         include "./rodape.inc";
---
+++ respostas.php
@@ -0,0 +1,20 @@
+<?php
+    require "./perguntas.inc";
+    $id = $_POST["id"];
+    $alternativa = $_POST["forms"];
+    $resposta = $_POST["resposta"];
+
+    if ($alternativa == $resposta){
+        if(($id + 1) == count($GLOBALS["alternativas"])){
+            header("Location: venceu.php");    
+        }else{
+            header("Location: perguntas.php?id=" . $id + 1);
+        }
+    }else{
+        header("Location: gameover.php");
+    }
+
+
+
+
+?>
