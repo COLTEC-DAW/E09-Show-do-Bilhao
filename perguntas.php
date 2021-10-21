c6a535e2bc3eebbb55e8e4aecc9caf55e2aedd0f
pedroping <2019951899@teiacoltec.org>
2021-10-21 18:04:04 -0300

    Identificando o jogador (otmizada corrigida)

--- perguntas.php
+++ perguntas.php
@@ -8,8 +8,11 @@
     if(isset($_POST["login"]) && isset($_POST["senha"])){
         $_SESSION['login'] = $_POST["login"];
         $_SESSION['senha'] = $_POST["senha"];
+        if(!isset($_COOKIE["nome"]) || !isset($_COOKIE["pontos"])){
+            setcookie("nome", $_SESSION['login'], time()+3600);
+            setcookie("pontos", $_SESSION['pontos'], time()+3600);
+        }
     }
-    
 ?>
 
 <!DOCTYPE html>
