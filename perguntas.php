e3816621840fdf66ce02ab6c36627d83c2f18f0e
Amanda Bueno <amandabuenocampos@gmail.com>
2022-07-10 23:19:30 -0300

    separando uma pergunta por pagina

--- perguntas.php
+++ perguntas.php
@@ -4,9 +4,22 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Perguntas</title>
+        <title>Show do Bilhão</title>
     </head>
     <body>
+        <?php 
+            $menu = "partials/menu.inc";
+            $footer = "partials/rodape.inc";
+            $questions = "partials/perguntas.inc";
 
+            if (is_readable($menu)) include $menu;
+            if (is_readable($questions)) include $questions;
+            else echo "Não foi possível carregar as perguntas.";
+
+            if ((isset($_GET["id"])) && (($_GET["id"]) <= count($GLOBALS["questions"])) && ($_GET["id"]) >= 0) carregaPergunta($_GET["id"]);
+            else echo "ID inválido";
+
+            if (is_readable($footer)) include $footer;
+        ?>
     </body> 
 </html>
