22bbd2125baa173859d52c1d887576ccbc2f3bb2
Dannillouou <danielalmemiranda@gmail.com>
2022-07-20 21:37:24 -0300

    retirado script.js

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
   <!--CSS-->
-  <link rel="stylesheet" href="style.css"/>
+  <link rel="stylesheet" href="style.css" />
 
   <title>Show do Bilhão</title>
 </head>
+
 <body>
-  
+
 
   <?php
 
@@ -32,6 +32,6 @@
 
   ?>
 
-
 </body>
+
 </html>
