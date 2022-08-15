cf96e21bf13fdbf18d9b3290c1aa8a1d823a5db7
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-08-14 21:30:28 -0300

    alterações

--- nextPage.php
+++ nextPage.php
@@ -10,11 +10,12 @@
 
     <?php 
 
-        include 'perguntas.php';
+        $conteudo = 'perguntas.php';
         $menu = "menu.inc";
         $enunciados = "perguntas.inc";
         $rodape = "rodape.inc";
 
+        if (is_readable($conteudo)) include $conteudo;
         if (is_readable($menu)) include $menu;
         if (is_readable($enunciados)) include $enunciados;
 
@@ -22,14 +23,15 @@
 
             if($_POST['question'] == count($perguntas) - 1){
 
-                header("Location: ganhou.html", TRUE, 301);
+                header("Location: ganhou.html");
 
             }
 
             carregaPergunta($_POST['question'] + 1, $perguntas, $alternativas);
 
         } else {
-            header("Location: perdeu.html", TRUE, 301);
+
+            header("Location: perdeu.html");
         }
 
         if (is_readable($rodape)) include $rodape;
