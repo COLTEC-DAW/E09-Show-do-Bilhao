d7989d10790d2ad5c04d628a8cc04b7c8947dd6a
wanessamarcos <102993709+wanessamarcos@users.noreply.github.com>
2022-08-21 14:09:51 -0300

    Checando Respostas

---
+++ ChecandoRespostas.php
@@ -0,0 +1,15 @@
+<?php
+    $resposta = $_POST["resposta"];
+    $perguntaAtual = $_POST["pergunta"];
+    $alternativaCorreta = $_POST["respostascorretas"];
+    if($perguntaAtual == 3 && $resposta == $alternativaCorreta ){
+        header("Location: http://localhost/O-Show-do-Bilhao/Ganhou.php"); 
+    }
+    else if($resposta == $alternativaCorreta){
+        header("Location: http://localhost/O-Show-do-Bilhao/perguntas.php?id=".($perguntaAtual+1)."");
+    }
+    else{
+        header("Location: http://localhost/O-Show-do-Bilhao/GameOver.php");
+    
+    }
+?>
---
+++ Ganhou.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Show do Bilhão</title>
+</head>
+<body>
+    <div>
+        <?php include "menu.inc"; ?>
+    </div>
+
+    <div>
+        <h3> VOCÊ GANHOUUUUUUUUUU !!! PARABÉNS !!!!</h3>
+    </div>
+
+    <div>
+        <?php include "rodape.inc"; ?>
+    </div>
+</body>
+</html>
