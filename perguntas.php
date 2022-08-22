4cd6d4b0b431c41646622a14013b19071f58145a
Amanda Bueno <amandabuenocampos@gmail.com>
2022-08-22 08:20:36 -0300

    Checando respostas & Identificando o jogador

--- perguntas.php
+++ perguntas.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,11 +16,36 @@
             if (is_readable($menu)) include $menu; 
         ?>
         <?php
+            //if ((isset($_GET["id"])) && (($_GET["id"]) <= count($GLOBALS["questions"])) && ($_GET["id"]) >= 0) carregaPergunta($_GET["id"]);
+            //else echo "ID inválido";
+
+            session_start();
+            //session_regenerate_id();
+            if(!isset($_SESSION['username'])){
+                header("Location: cadastro.php");
+            }
+
+            $rightAnswers = $_COOKIE['rightAnswers'];
+
             if (is_readable($questions)) require $questions;
             else echo "Não foi possível carregar as perguntas.";
 
-            if ((isset($_GET["id"])) && (($_GET["id"]) <= count($GLOBALS["questions"])) && ($_GET["id"]) >= 0) carregaPergunta($_GET["id"]);
-            else echo "ID inválido";
+            $question = array_keys($questions, $_POST['question']);
+            
+            if($correctAnswers[$question] == $_POST['answer']){
+                $rightAnswers++;
+                $_COOKIE['rightAnswers'] = $rightAnswers;
+                if($_POST['question'] == count($questions) - 1){
+                    header("Location: ganhou.php");
+                    exit(1);
+                }
+                carregaPergunta($_POST['question'] + 1);
+                echo "Acertos: {$rightAnswers}";
+            } else {
+                header("Location: perdeu.php");
+            }
+        
+            if (is_readable($logout)) include $logout;
         ?>
         <?php
             if (is_readable($footer)) include $footer;
