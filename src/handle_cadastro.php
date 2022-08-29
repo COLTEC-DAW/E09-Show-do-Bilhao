4cd64a414b7eadd1fc5b9ab66f1d2ed0d065e98f
Gustavo Rangel <78743601+Aikenpil@users.noreply.github.com>
2022-08-29 17:10:19 -0300

    feat: insert_into_json error fix

--- src/handle_cadastro.php
+++ src/handle_cadastro.php
@@ -1,14 +1,15 @@
 <?php
     require "User.php";
-    function insert_into_json($nome_arquivo, $data){
+    function insert_into_json($nome_arquivo, User $data){
         $old_data = json_decode(file_get_contents($nome_arquivo));
         if(!isset($old_data)) $data_to_append = $data;
-        else $data_to_append = json_decode($data);
+        else $data_to_append = json_encode($data);
+        $data_to_append = array($data_to_append);
         array_push($data_to_append, $data);
         file_put_contents($nome_arquivo, json_encode($old_data, JSON_PRETTY_PRINT), LOCK_EX);
     }
 
-    $user = new User($_POST["login"], $_POST["senha"], $_POST["email"], $_POST["nome"]);
+    $user = new User($_POST['login'], $_POST['senha'], $_POST['email'], $_POST['nome']);
 
     insert_into_json("usuarios.json", $user);
 
