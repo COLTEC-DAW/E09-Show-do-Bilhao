0bd4f42b44fcd20e54fb171ec80042797291d7d5
Aikenpil <gustebas@hotmail.com>
2022-08-30 13:41:04 -0300

    feat: "signed in" verification and JSON insert fix

--- src/handle_cadastro.php
+++ src/handle_cadastro.php
@@ -1,17 +1,42 @@
 <?php
     require "User.php";
+    
+    define("USUARIO_CADASTRADO", TRUE);
+    define("USUARIO_NAO_CADASTRADO", FALSE);
+
+    function verify_signed_user($nome_arquivo, User $data){
+
+        $arquivo_usuarios = json_decode(file_get_contents($nome_arquivo));
+        if($arquivo_usuarios == null) return USUARIO_NAO_CADASTRADO;
+        foreach ($arquivo_usuarios as $usuarios){
+            if($usuarios->login == $data->login){
+                return USUARIO_CADASTRADO;
+            }
+        }
+        return USUARIO_NAO_CADASTRADO;
+    }
+    
     function insert_into_json($nome_arquivo, User $data){
         $old_data = json_decode(file_get_contents($nome_arquivo));
-        if(!isset($old_data)) $data_to_append = $data;
-        else $data_to_append = json_encode($data);
-        $data_to_append = array($data_to_append);
-        array_push($data_to_append, $data);
-        file_put_contents($nome_arquivo, json_encode($old_data, JSON_PRETTY_PRINT), LOCK_EX);
+        if(isset($old_data)) $data_to_append = $old_data;
+        else $data_to_append = $data;
+        array($data_to_append);
+        $user = verify_signed_user($nome_arquivo, $data);
+        if(!$user) {
+            array_push($data_to_append, $data);
+            file_put_contents($nome_arquivo, json_encode($data_to_append, JSON_PRETTY_PRINT), LOCK_EX);
+        } else return USUARIO_CADASTRADO;
     }
 
     $user = new User($_POST['login'], $_POST['senha'], $_POST['email'], $_POST['nome']);
 
-    insert_into_json("usuarios.json", $user);
+    $verification = insert_into_json("usuarios.json", $user);
+
+    if($verification) {
+        echo "Login já cadastrado";
+        echo "<br><a href='cadastro_usuario.php'><button>Voltar para pagina de cadastro</button></a>";
+        exit(1);
+    }
 
     header("Location: login.php", TRUE, 301);
 ?>
