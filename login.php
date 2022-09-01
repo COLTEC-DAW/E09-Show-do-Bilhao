42ce332dc1d3aee0fc670a2c0d7fc1a6ecb6e148
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-09-01 20:44:08 -0300

    testando o login

--- login.php
+++ login.php
@@ -1,13 +1,33 @@
 <?php
-$redirect_to = "index.php";
 
-// se POST, então usuário quer realizar login
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    session_start();
-    
-    $_SESSION["user"] = $_POST["login"];
-    $redirect_to = "pergunta.php?id=0";
-}
+    function VerificaLogin($fileName, $login, $password){
 
-// redireciona para a página após verificações
-header("Location: $redirect_to");
+        $fileUsers = json_decode(file_get_contents($fileName));
+
+        foreach ($fileUsers as $usuarios){
+        if($usuarios->password == $password && $usuarios->login == $login){
+            return TRUE;
+        }
+        }
+        return FALSE;
+
+    }
+
+    $redirect_to = "index.php";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        session_start();
+
+        if(VerificaLogin('json/usuarios.json', $_POST['login'], $_POST['password']) == TRUE){
+
+            $_SESSION["user"] = $_POST["login"];
+            $redirect_to = "pergunta.php?id=0";
+            header("Location: $redirect_to");
+
+        }else{
+
+            header("Location: $redirect_to");
+        }
+    }
+
+?>
