9d9289dc2b05a99a492d2090387c8e074eb06166
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-09-01 23:20:29 -0300

    4 possibilidades da não existência de um login

---
+++ senhaInvalida.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">
+<div class="content">
+    
+    <h2>SENHA INVÁLIDA</h2>
+    <br>
+<a href="index.php"><button>Voltar para painel de login.</button></a>
+</div>
+</div>
+</body>
+</html>
