46df7efc17b3498c5d743df207f9825e8c182a55
wanessamarcos <102993709+wanessamarcos@users.noreply.github.com>
2022-09-04 10:37:10 -0300

    completo

--- autenticador.php
+++ autenticador.php
@@ -1,24 +1,17 @@
 <?php
 session_start();
-
-
 try{
-    $usuario_certo = "wanessa";
-    $senha_certa = "123";
-    
-    $usuario_digitado = $_POST["user"];
-    $senha_digitada = $_POST["pass"];
-    
-    if($usuario_digitado == $usuario_certo){
-        if($senha_digitada == $senha_certa){
-            $_SESSION["usuario_logado"] = $usuario_digitado;
-            header("Location: perguntas.php?id=0");
-        } else {
-            header("Location: login.php?falhou=true");
+    $users = @json_decode(file_get_contents('users.json'), true);
+    $usuario['login'] = $_POST["user[login]"];
+    $usuario['senha'] = $_POST["user[senha]"];
+    foreach ($users as $user){
+            if($users['login'] == $usuario['login']){
+                if($users['senha'] == $usuario['senha']){
+                    $_SESSION["usuario_logado"] = $user['login'];
+                    header("Location: perguntas.php?id=0");
+                }
+            }
         }
-    }else {
-        header("Location: login.php?falhou=true");
-    }
 }catch(Exception $e){
     echo $e->getMessage();
 }
