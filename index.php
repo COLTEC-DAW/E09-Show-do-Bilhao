dcac5b786f99c4783c4ebcba6b9369c9bafdb3c4
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2023-05-26 10:42:17 -0300

    Funcional, mas sem perguntas

---
+++ index.php
@@ -0,0 +1,51 @@
+
+<?php
+include "perguntas.inc";
+
+$id = $_GET['id'] - 1;
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Show do Bilhão</title>
+    </head>
+
+    <body>
+        <?php include "menu.inc"?>
+
+        <main>
+            <?php
+                $dadosPergunta = carregaPergunta($id);
+
+                $pergunta = $dadosPergunta[0];
+                $alt = $dadosPergunta[1];
+                $resposta = $dadosPergunta[2];
+            ?>
+
+            <div class="questao">
+                <h2 class="enunciado"> <?php echo $pergunta ?> </h2>
+                <?php
+                foreach($alt as $index => $alternativa){
+
+                    echo "<input type='radio' name='alt' id='alt-$index'>
+                    <label for='alt-$index'> $alternativa </label><br>";
+
+                }
+                ?>
+                
+
+            </div>
+            
+        </main>
+
+        <?php include "rodape.inc"?>
+    </body>
+
+</html>
