fe5407ce6c124168be80ca61970f065ac211f943
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-05-28 00:41:31 -0300

    criando array de alternativas corretas e mostrando gabarito

--- index.php
+++ index.php
@@ -10,12 +10,20 @@
 <?php 
     $enunciados=array("Qual bicho transmite a Doença de Chagas?", "Qual fruto é no Norte e Nordeste conhecido como jerimum?", "Qual é o coletivo de cães?", "Qual pe o triângulo que tem todos os lados diferentes?", "Quem compôs o hino da independência?");
     $alternativas = array(array("abelha", "barata", "pulga", "barbeiro"), array("caju", "abobora", "chuchu", "côco"), array("matilha", "rebanho", "alcateia", "manada"), array("equilátero", "isósceles", "escaleno", "trapézio"), array("Dom Pedro I.", "Manuel Bandeira", "Castro Alves", "Carlos Gomes"));
+    $alternativasCorretas = array(3, 1, 0, 2, 0);
     for($i=0;$i<5;$i++){
         echo "$enunciados[$i] <br>";
         for($j=0;$j<4;$j++){
             echo $alternativas[$i][$j]; 
             echo "<br>"; 
         }
+    }
+    
+    echo "GABARITO <br>";
+    for($i=0;$i<5;$i++){
+        echo "Questão $i: ";
+        echo $alternativas[$i][$alternativasCorretas[$i]];
+        echo "<br>";
     }
 ?>
 </body>
