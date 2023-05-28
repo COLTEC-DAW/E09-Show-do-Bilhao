28bd612b1f3f00ffb413a18b9ac256829db13cf1
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-05-28 00:40:41 -0300

    criando e mostrando matriz de alternativas

--- index.php
+++ index.php
@@ -9,8 +9,13 @@
 <body>
 <?php 
     $enunciados=array("Qual bicho transmite a Doença de Chagas?", "Qual fruto é no Norte e Nordeste conhecido como jerimum?", "Qual é o coletivo de cães?", "Qual pe o triângulo que tem todos os lados diferentes?", "Quem compôs o hino da independência?");
+    $alternativas = array(array("abelha", "barata", "pulga", "barbeiro"), array("caju", "abobora", "chuchu", "côco"), array("matilha", "rebanho", "alcateia", "manada"), array("equilátero", "isósceles", "escaleno", "trapézio"), array("Dom Pedro I.", "Manuel Bandeira", "Castro Alves", "Carlos Gomes"));
     for($i=0;$i<5;$i++){
         echo "$enunciados[$i] <br>";
+        for($j=0;$j<4;$j++){
+            echo $alternativas[$i][$j]; 
+            echo "<br>"; 
+        }
     }
 ?>
 </body>
