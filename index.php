1edb05a3fa6482cf5212d61692c6c587ff58e850
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2023-05-28 21:51:33 -0300

    Update

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 
 <?php
-include "perguntas.inc";
+require "perguntas.inc";
 
-$id = $_GET['id'] - 1;
+if(count($_GET) == 0){
+    $id = 0;
+}else{
+    $id = $_GET['id'];
+}
+
 
 $dadosPergunta = carregaPergunta($id);
 
 $pergunta = $dadosPergunta[0];
 $alt = $dadosPergunta[1];
 $resposta = $dadosPergunta[2];
-
 ?>
 
 
@@ -20,7 +24,10 @@ $resposta = $dadosPergunta[2];
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pergunta <?php echo ($id + 1) ?> - Show do Bilhão</title>
+        <title>Pergunta <?php echo ($id + 1) ?> - Show dos Otakus</title>
+
+        <link rel="stylesheet" href="reset.css">
+        <link rel="stylesheet" href="style.css">
     </head>
 
     <body>
@@ -29,12 +36,19 @@ $resposta = $dadosPergunta[2];
         <main>
             <div class="questao">
                 <h2 class="enunciado"> <?php echo $pergunta ?> </h2>
-                <?php foreach($alt as $index => $alternativa){ ?>
 
-                    <input type='radio' name='alt' id='alt-<?php echo $index ?>'>
-                    <label for='alt-<?php echo $index ?>'> <?php echo $alternativa ?> </label><br>
+                <div class="alternativas">
+                    <?php foreach($alt as $index => $alternativa){ ?>
+                    
+                    <div class="alt">
+                        <input type='radio' name='alt' id='alt-<?php echo $index ?>'>
+                        <label for='alt-<?php echo $index ?>'> <?php echo $alternativa ?> </label><br>
+                    </div>
 
-                <?php } ?>
+                    <?php } ?>
+
+                </div>
+                
                 
             </div>
             
