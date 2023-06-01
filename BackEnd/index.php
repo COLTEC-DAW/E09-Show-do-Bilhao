9f77c89684eaf2247a3744d6da7e6faa555aa200
Daprats <danielaugustoprates@gmail.com>
2023-06-01 12:59:40 -0300

    Update

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -1,27 +1,26 @@
 <?php 
     
-$alternatives = array(
-    $alternative1 = "a",
-    $alternative2 = "b",
-    $alternative3 = "c",
-    $alternative4 = "d",
-);
-
     class Questions{
         public $question;
         public $alternatives;
         public $rightAlternatives;
 
+    public function __construct($question, $alternatives, $rightAlternatives){
+        $this->question = $question;
+        $this->alternatives = $alternatives;
+        $this->rightAlternatives = $rightAlternatives;
+    }
+
     public function Imprime(){
-        echo "Pergunta: ".$this->question."Alternativas: ".$this->alternatives."<br>Alternativa Certa: ".$this->rightAlternatives;
+        echo "Pergunta: ".$this->question."<br>";
+        print_r($this->alternatives);
+        echo "<br>Alternativa Certa: ".$this->rightAlternatives;
     }
 
     }
 
-$pergunta1 = new Questions();
-$pergunta1->question = "Qual o piriri do pororo?";
-$pergunta1->rightAlternatives = "C";
-$pergunta1->alternatives[0] = "cacete"; 
-var_dump($pergunta1);
+$alternatives = array();
 
+$pergunta1 = new Questions("Qual animal come banana?",array("O macaco", "A Girafa", "A Abelha", "O Passaro"),"1");
+$pergunta1->Imprime();
 ?>
