98d1ef8316dac2c9ea82611db547f884fefefa5f
ArthurMacielSs <2021952422@teiacoltec.org>
2023-06-01 10:59:53 -0300

    começo do primeiro arquivo

---
+++ index.php
@@ -0,0 +1,54 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> ou</h1>
+    <?php
+    class questions {
+        public string $enunciado;
+        public $questões;
+        public string $questãocerta;
+    }
+    $perguntas = array(
+        "pergunta1"=> new questions(),
+        "pergunta2"=> new questions(),
+        "pergunta3"=>new questions(),
+        "pergunta4"=>new questions(),
+        "pergunta5"=>new questions()
+    );
+
+    $perguntas[pergunta1]->questões =array(
+        "a"=> "oioioioi",
+        "b"=> "oioioioiio",
+        "c"=> "oioi",
+        "d"=>"oioioi"
+    );
+    $perguntas[pergunta2]->questões =array(
+        "a"=> "oioioioi",
+        "b"=> "oioioioiio",
+        "c"=> "oioi",
+        "d"=>"oioioi"
+    );
+    $perguntas[pergunta3];
+    $perguntas[pergunta4];
+    $perguntas[pergunta5];
+
+    ?>
+
+    
+
+    
+</body>
+</html>
+
+$array = array(
+ “chave1” => 1,
+ “chave2” => “PHP”,
+ “chave3” => false
+ );
+
+ var_dump();
