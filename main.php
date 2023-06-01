594975f9f8851bb44d3912f8f3b3f184c4d62f23
Caesar <2021951620@teiacoltec.org>
2023-06-01 11:05:03 -0300

    ADD: Question system & Style (early)

--- main.php
+++ main.php
@@ -1,20 +1,18 @@
 <html>
 <head>
 	<title>My first PHP script</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    Agent: So who do you think you are, anyhow?
+    <h1> Vai começar o Show do Milho </h1>
     <br>
     <?php
-        require('question.php');
-        $db1 = new Question('Qual a capital do Acre?', array('a', 'b', 'c'), 'c');
-        $db2 = new Question('Qual a capital do Arizona?', array('a', 'b', 'c'), 'a');
+        require('data.inc.php');
 
-        echo "WTF?";
-        echo " $db1->id ";
-        echo " $db2->id ";
-        // Print output
-        echo 'Neo: I am Neo, but my people call me The One.';
+        foreach($questions as $quest)
+        {
+            $quest->ShowQuestion();
+        }
     ?>
 </body>
 </html>
