1d2f7432b29f9bdf659821fb27b01def31a6dd3e
Caesar <2021951620@teiacoltec.org>
2023-06-02 08:48:18 -0300

    FIX: Footer pos

---
+++ footer.inc.php
@@ -0,0 +1,3 @@
+<php?
+  echo '<div class="footer"> <h3>Caio César [CostaCesar] 2023 </h3> </div>';
+?>
---
+++ menu.inc.php
@@ -0,0 +1,3 @@
+<php?
+  echo '<div class="footer"> <h3>Caio César [CostaCesar] 2023©</h3> </div>';
+?>
