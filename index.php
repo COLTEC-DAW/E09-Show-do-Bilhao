8a4d6364ae68143157a97f304360ea238226f851
ArthurMacielSs <2021952422@teiacoltec.org>
2023-06-02 10:54:53 -0300

    fim parte 1

--- index.php
+++ index.php
@@ -8,47 +8,45 @@
 <body>
     <h1> ou</h1>
     <?php
+    class alternativas {
+        public string $alternativaA,$alternativaB,$alternativaC,$alternativaD;
+        
+    }
+
     class questions {
         public string $enunciado;
         public $questões;
-        public string $questãocerta;
+        public string $questaocerta;
+
+
+        public function __construct($enunciado,$alternativaA,$alternativaB,$alternativaC,$alternativaD,$questaocerta){
+            $this->enunciado=$enunciado;
+            $this->questões=array(1=> $alternativaA,2=> $alternativaB, 3=> $alternativaC,4=>$alternativaD,);
+            $this->questaocerta=$questaocerta;
+        }
+        public function MostraQuestões($numero){
+        echo"<br> questão: ".$numero."<br>";
+            echo ($this->enunciado."<br>");
+            foreach($this->questões as $questao){
+                echo $questao."<br>";
+            }
+            echo ("a alternativa certa é a:" .$this->questaocerta."<br>");  
+        }
     }
     $perguntas = array(
-        "pergunta1"=> new questions(),
-        "pergunta2"=> new questions(),
-        "pergunta3"=>new questions(),
-        "pergunta4"=>new questions(),
-        "pergunta5"=>new questions()
+        1=> new questions("qual o tamanho do meu pau?","A 15 cm","B 22 cm","C 5 cm","D 6 cm","a"),
+        2=> new questions("qual o objeto que se bebe café?", "garrafa vodka","bbb","ccc","ddd","a"),
+        3=>new questions("qual o objeto que se bebe café?", "garrafa vodka","bbb","ccc","ddd","a"),
+        4=>new questions("qual o objeto que se bebe café?", "garrafa vodka","bbb","ccc","ddd","a"),
+        5=>new questions("qual o objeto que se bebe café?", "garrafa vodka","bbb","ccc","ddd","a")
     );
 
-    $perguntas[pergunta1]->questões =array(
-        "a"=> "oioioioi",
-        "b"=> "oioioioiio",
-        "c"=> "oioi",
-        "d"=>"oioioi"
-    );
-    $perguntas[pergunta2]->questões =array(
-        "a"=> "oioioioi",
-        "b"=> "oioioioiio",
-        "c"=> "oioi",
-        "d"=>"oioioi"
-    );
-    $perguntas[pergunta3];
-    $perguntas[pergunta4];
-    $perguntas[pergunta5];
-
-    ?>
-
-    
-
-    
+    for ($i = 1; $i <= count($perguntas); $i++) {
+    $perguntas[$i]->MostraQuestões($i);
+    }
+    ?>  
 </body>
 </html>
 
-$array = array(
- “chave1” => 1,
- “chave2” => “PHP”,
- “chave3” => false
- );
 
- var_dump();
+
