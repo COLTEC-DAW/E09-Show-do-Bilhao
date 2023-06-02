5d774b10302b5fd52833d890f1db203bf1a1d875
Leonardo <104566026+LeoPradoOliveira@users.noreply.github.com>
2023-06-02 10:06:43 -0300

    Add files via upload

---
+++ Question.php
@@ -0,0 +1,14 @@
+<?php
+    final class Question{
+        public $question;
+        public $options;
+        public $answer;
+
+        public function __construct($question, $options, $answer){
+            $this->question = $question;
+            $this->options = $options;
+            $this->answer = $answer;
+        }
+    }
+
+?>
---
+++ question.inc
@@ -0,0 +1,8 @@
+<?php
+require "Question.php";
+
+function load_question($id, $fileName){
+    $file = json_decode(file_get_contents($fileName));
+    return new Question($file[$id]->question, $file[$id]->options, $file[$id]->answer);
+}
+?>
