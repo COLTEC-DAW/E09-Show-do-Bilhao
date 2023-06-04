77442fb93a3b4b795763a5d36b5dfbf183f70201
Igor Barroso Almeida <igorba867@gmail.com>
2023-06-04 16:45:46 -0300

    Checando respostas

--- index.php
+++ index.php
@@ -13,22 +13,25 @@
     <?php
         $game = new PerguntaMaker();
 
+        // Se id veio na requisição a variavel assume tal valor
         if(isset($_GET["id"])) {
             $id = $_GET["id"];
         }
+        // Se não, é a primeira questão
         else {
             $id = 0;
         }
     ?>
     
-    <form action="index.php?id=<?php echo $id+1?>" method="POST">
-        <label for="resp">
-            <?php
-                $game->carregaPergunta($id);
-            ?>
-        </label>
-        
-        <input type="submit" name="Confirmar resposta" id="BotaoEnviar">
-    </form>
+    <?php
+        // primeira pergunta ou acertou a anterior
+        if($id == 0 || $game->buscaAlternativaCorreta($id - 1) == $_POST["resp"]) {
+            $game->carregaPergunta($id);
+        }
+        // se errou carrega pagina de gameover
+        else {
+            echo "GAME OVER";
+        }
+    ?>
 </body>
 </html>
