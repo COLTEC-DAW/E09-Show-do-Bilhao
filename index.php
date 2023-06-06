92c3c52c533196a6837b41841ece60086acda834
Igor Barroso Almeida <igorba867@gmail.com>
2023-06-05 21:27:40 -0300

    estilização inicial

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!-- Importações -->
-<?php require "PerguntaMaker.inc" ?>
+<?php require "./assets/classes/PerguntaMaker.inc" ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,31 +7,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- css -->
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    
     <title>Show do Bilhão</title>
 </head>
 <body>
-    <?php
-        $game = new PerguntaMaker();
+    <header class="cabecalho">
+        <img src="./assets/img/logo.png" alt="Logo show do Bilhão" class="cabecalho--img">
+    </header>
 
-        // Se id veio na requisição a variavel assume tal valor
-        if(isset($_GET["id"])) {
-            $id = $_GET["id"];
-        }
-        // Se não, é a primeira questão
-        else {
-            $id = 0;
-        }
-    ?>
-    
-    <?php
-        // primeira pergunta ou acertou a anterior
-        if($id == 0 || $game->buscaAlternativaCorreta($id - 1) == $_POST["resp"]) {
-            $game->carregaPergunta($id);
-        }
-        // se errou carrega pagina de gameover
-        else {
-            echo "GAME OVER";
-        }
-    ?>
+    <main>
+        <div class="pergunta">
+            <?php
+                $game = new PerguntaMaker();
+
+                // Se id veio na requisição a variavel assume tal valor
+                if(isset($_GET["id"])) {
+                    $id = $_GET["id"];
+                }
+                // Se não, é a primeira questão
+                else {
+                    $id = 0;
+                }
+            ?>
+            
+            <?php
+                // primeira pergunta ou acertou a anterior
+                if($id == 0 || $game->buscaAlternativaCorreta($id - 1) == $_POST["resp"]) {
+                    $game->carregaPergunta($id);
+                }
+                // se errou carrega pagina de gameover
+                else {
+                    echo "GAME OVER";
+                }
+            ?>
+
+        </div>
+    </main>
+
 </body>
 </html>
