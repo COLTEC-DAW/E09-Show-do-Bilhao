2aadc615d1f032bffec57098272d90b41f15e2d9
lorenaricoy <lricoymartins@gmail.com>
2023-06-16 09:40:10 -0300

    mostrando perguntas

--- index.php
+++ index.php
@@ -5,33 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Show do bilhão</title>
-
-    <style>
-        h1{
-            color: pink;
-            text-align: center;
-        }
-        h2{
-            text-align: center;
-        }
-        .pergunta{
-            margin-top: 5px;
-        }
-        form{
-            width: 100%;
-            text-align: center;
-        }
-        footer{
-        background-color: pink;
-        width: 100%;
-        height: 50px;
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        text-align: center;
-        }
-        
-    </style>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <?php include "menu.inc"; ?>
@@ -44,7 +19,15 @@
     }
     ?>
 
-    <?php include "perguntas.php"; ?>
+    <?php
+        include "perguntas.inc";
+        carregaPergunta($id);
+    ?>
+
+    <?php 
+        
+    ?>
+
 
     <?php include "rodape.inc"; ?>
 </body>
