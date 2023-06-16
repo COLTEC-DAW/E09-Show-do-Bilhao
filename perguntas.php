b263cf0048081cbd3ce0cb9c2d29c35fe307bda0
Θαλησ <104565512+Thalesed@users.noreply.github.com>
2023-06-16 10:51:33 -0300

    Add files via upload

--- perguntas.php
+++ perguntas.php
@@ -1,36 +1,36 @@
 <?php
 $perguntas = array(
-    "pergunta1",
-    "pergunta2",
-    "pergunta3",
-    "pergunta4",
+    "Qual economista do século XIX desenvolveu a teoria das vantagens comparativas e argumentou que o trabalho era a fonte fundamental de valor na economia?",
+    "Qual é a caracteristica da democracia defendida por Montesquieu?",
+    "Qual das características não estão presentes na sociedade de 'A Utopia' de Thomas More?",
+    "Qual dos conceitos não foi desenvolvido por Karl Marx?",
     "pergunta5",
     "pergunta6"
 );
 $alternativas = array(
     array(
-        "a",
-        "b",
-        "c",
-        "d"
+        "a) David Ricardo.......",
+        "b) John Maynard Keynes",
+        "c) Karl Marx...........",
+        "d) Adam Smith.........."
     ),
     array(
-        "a",
-        "b",
-        "c",
-        "d"
+        "a) ao status de cidadania que o indivíduo adquire ao tomar as decisões por si mesmo. ",
+        "b) à possibilidade de o cidadão participar no poder e, nesse caso, livre da submissão às leis. ",
+        "c) ao direito do cidadão exercer sua vontade de acordo com seus valores pessoais",
+        "d) ao condicionamento da liberdade dos cidadãos à conformidade às leis. "
     ),
     array(
-        "a",
-        "b",
-        "c",
-        "d"
+        "a) Igualdade Social e econômica",
+        "b) Participação política. . . . . . . . .",
+        "c) Direito a propriedade privada",
+        "d) Liberdade religiosa. . . . . . . . . ."
     ),
     array(
-        "a",
-        "b",
-        "c",
-        "d"
+        "a) Luta de classes. . . . . . . ",
+        "b) Mais valia. . . . . . . . . .",
+        "c) Fetichismo da mercadoria. . .",
+        "d) Teoria da vantagem comparativa"
     ),
     array(
         "a",
