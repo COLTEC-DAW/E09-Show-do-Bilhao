bb35bb20ea87d59e07716fb3a5f15b8d8499def1
EduardoBirchal <edubirchal@gmail.com>
2023-06-23 10:42:42 -0300

    Teste (tava comittando como se eu fosse o Erick Pedrosa)

--- paginaResultado.inc.php
+++ paginaResultado.inc.php
@@ -9,7 +9,8 @@
 
     function respostaErrada(int $idPagina, int $idRespostaCorreta, string $stringRespostaCorreta) {
         $idRespostaCorreta++;
-        
+        // Os IDs começam no 0, mas é mais intuitivo pro usuário apresentá-los como se começassem no 1
+
         echo("<h1>Resposta errada!</h1>");
         echo("Resposta correta: $stringRespostaCorreta (alternativa $idRespostaCorreta)");
     }
