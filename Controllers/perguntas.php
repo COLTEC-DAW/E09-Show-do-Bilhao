5683d34839cafe75004f73c64cb98996c0c0ac6c
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-06-25 14:30:45 -0300

    buscando perguntas do banco de dados

--- Controllers/perguntas.php
+++ Controllers/perguntas.php
@@ -4,35 +4,35 @@ require "../Models/Perguntas.inc";
 <?php 
 require "../Models/User.inc";
 ?>
-
 <?php 
-
 session_start();
 if(isset($_SESSION['user'])){
     if($_SERVER["REQUEST_METHOD"] === "POST"){
         global $alternativas;
         global $enunciados;
+        global $perguntas;
         if(!isset($_POST["opcao"])){
             $id=0;
             $pontuacao=0;
-            $pergunta= carregaPerguntas($id);
+            $pergunta= carregaPerguntas($id, $perguntas);
             require "../Componentes/pergunta.inc";
             
         }else {
             $resposta =  htmlspecialchars($_POST["opcao"]);
             $pergunta = htmlspecialchars($_POST["pergunta"]);
             $pontuacao = htmlspecialchars($_POST["pontuacao"]);
-            if($resposta==$alternativasCorretas[$pergunta]){
+
+            if($resposta==$perguntas[$pergunta]['resposta']){
 
             $pontuacao++;
        
-            if($pergunta==sizeof($enunciados)-1){
+            if($pergunta==sizeof($perguntas)-1){
                 criaUsuarioECookie($_SESSION["user"],$_SESSION["senha"], $pontuacao );
                 require "../Pages/voceGanhou.php";
             }
             else{
                 $id=$pergunta+1;
-                $pergunta= carregaPerguntas($id);
+                $pergunta= carregaPerguntas($id, $perguntas);
                 require "../Componentes/pergunta.inc";
             }
         }else{
