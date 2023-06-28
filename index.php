0b51d6b2df679bdc56353d3d435f1b9b4fd9ceb6
Igor Barroso Almeida <igorba867@gmail.com>
2023-06-28 17:26:24 -0300

    mudando logica

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
     <main>
         <div class="pergunta">
             <?php
+                //Processamento
                 $game = new Gerenciador();
 
                 // Se id veio na requisição a variavel assume tal valor
@@ -31,20 +32,71 @@
                 else {
                     $id = 0;
                 }
+
+                $perguntaAtual = $game->carregaPergunta($id);                
             ?>
-            
-            <?php
-                // primeira pergunta ou acertou a anterior
-                if($id == 0 || $game->buscaAlternativaCorreta($id - 1) == $_POST["resp"]) {
-                    $game->carregaPergunta($id);
-                }
-                // se errou carrega pagina de gameover
-                else {
+
+            <?php           
+                // Se não tem login ou senha aparece a pagina de login
+                if(!(isset($_SESSION["login"])) || !(isset($_SESSION["senha"]))) {
                     ?>
-                        <h3 class='game_over'>GAME OVER</h3>
+                        <!-- =============== -->
+                        <h1>Login</h1>
+
+                        <form action="index.php" method="POST">
+                            <label for="nome">Nome: </label>
+                            <input type="text">
+
+                            <label for="nome">Senha: </label>
+                            <input type="password">
+
+                            <input type="submit">
+                        </form>
+                        <!-- =============== -->
                     <?php
-                    $game->pontuacao = $id - 1;
+                } else {
+                    // primeira pergunta ou acertou a anterior
+                    if($id == 0 || $game->buscaAlternativaCorreta($id - 1) == $_POST["resp"]) {
+                        $nQuestao = $id+1;
+                        $count = 0;
+                        $game->pontuacao = $id;
+                        ?>
+                            <!-- =============== -->
+                            <h2 class='pergunta--titulo'>Questão <?= $nQuestao ?></h2>
+                            <p class='pergunta--enunciado'><?= $perguntaAtual->enunciado ?></p>
+                            
+                            <form action='index.php?id=<?= $nQuestao ?>' method='POST'>
+                            <!-- =============== -->                        
+                        <?php
+                        foreach($perguntaAtual->alternativas as $alternativa){
+                            ?>
+                                <!-- =============== -->                            
+                                <div class='alternativa'>
+                                <input type='radio' name='resp' id='resp$count' value='<?= $alternativa->letra ?>' class = 'input--alternativa'>
+                                <p class='non-selected'><?= $alternativa->letra ?>) <?= $alternativa->resposta ?></p>
+                                </div>
+                                <!-- =============== -->                            
+                            <?php
+                            $count++;
+                        }
+                        ?>
+                            <!-- =============== -->
+                            <input type='submit' value='Confirmar resposta' id='BotaoEnviar'>
+                            </form>
+                            <!-- =============== -->
+                        <?php
+                    }
+                    // se errou carrega pagina de gameover
+                    else {
+                        $game->pontuacao = $id - 1;
+                        ?>
+                            <!-- =============== -->
+                                <h3 class='game_over'>GAME OVER</h3>
+                            <!-- =============== -->
+                        <?php
+                    }
                 }
+
             ?>
 
         </div>
