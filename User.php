b32362d1878d3e9ac5b5ec7767da0c850cf33bab
Leonardo <104566026+LeoPradoOliveira@users.noreply.github.com>
2023-06-29 09:51:09 -0300

    .

--- User.php
+++ User.php
@@ -7,6 +7,7 @@
         private $password;
         private $highscore;
         private $foto;
+        private $last_session;
         private $storage = "../users/Usuarios.json";
         private $stored_users;
     
@@ -18,6 +19,7 @@
             $this->password = $password;
             $this->highscore = 0;
             $this->foto = rand(0, 17);
+            $this->last_session = date('d/m H:i');
             $this->stored_users = json_decode(file_get_contents($this->storage), true);
     }
 
@@ -29,7 +31,8 @@
                     'login' => $this->login,
                     'password' => $this->password,
                     'highscore' => $this->highscore,
-                    'foto' => $this->foto];
+                    'foto' => $this->foto,
+                    'last_session' => $this->last_session];
     }
 
     public function setHighscore($score){
@@ -43,9 +46,21 @@
 		}
     }
 
+    public function getHighscore($score){
+        for ($i=0; $i < count($this->stored_users); $i++) {
+			if($this->stored_users[$i]['login'] == $this->login){
+                return $this->stored_users[$i]['highscore'];
+			}
+		}
+    }
+
     public function setLastSession(){
-        $last_session = date('H:i:s');
-        setcookie('last_session', $last_session, time() + 86400, '/');
+        for ($i=0; $i < count($this->stored_users); $i++) {
+			if($this->stored_users[$i]['login'] == $this->login){
+                $this->stored_users[$i]['last_session'] = date('d/m H:i');
+                file_put_contents('../users/Usuarios.json', json_encode($this->stored_users, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+			}
+		}
     }
 }
 ?>
