52fa40365f45c64d74aed15e1697540ed198099b
ArthurMacielSs <2021952422@teiacoltec.org>
2023-06-30 11:20:26 -0300

    tentando fazer a parte 3

--- perguntas.php
+++ perguntas.php
@@ -1,3 +1,19 @@
+<?php
+    //chama o arquivo perguntas.inc e o menu.inc, so roda o site se ele existir 
+    require 'perguntas.inc';
+    require 'menu.inc';
+    //pega o id e armazena ele na posição id, isset verficia se o ide existe, e se ele existir retorna true, e o id é pego e armazenado em id, caso issso não aconteça ele armazena 1
+    $id = isset($_GET['id']) ? $_GET['id'] : 1;
+    //carrega as perguntas e usa essa função presente em perguntas .inc
+   //$pergunta=carregaPergunta($id,$perguntas);
+
+   if ($id <= count($perguntas)) {
+       $pergunta_atual = $perguntas[$id];
+   } else {
+       $id = count($perguntas);
+       $pergunta_atual = "Fim das perguntas.";
+   }
+   ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,92 +22,37 @@
     <title>Jogo do bilhão</title>
 </head>
 <body>
-    
-</body>
-</html>
+   
+       <h1>Pergunta <?php echo $id; ?></h1>
+       <p><?php $pergunta_atual->enunciado($id); ?></p>
+   
+       <?php if ($id < count($perguntas)): ?>
+        <form method="POST" action="perguntas.php">
+    <?php foreach ($pergunta_atual->questões as $questao): ?>
+        <input type="radio" name="resposta" value="<?php echo $questao; ?>">
+        <label><?php echo $questao; ?></label>
+        <br>
+    <?php endforeach; ?>
 
-    <?php
-    //chama o arquivo perguntas.inc, so roda o site se ele existir 
-    require 'perguntas.inc';
-
-    //pega o id e armazena ele na posição id, isset verficia se o ide existe, e se ele existir retorna true, e o id é pego e armazenado em id, caso issso não aconteça ele armazena 1
-    $id = isset($_GET['id']) ? $_GET['id'] : 1;
-
-    //classe questões 
-    class questions {
-        public string $enunciado;
-        public $questões;
-        public string $questaocerta;
-
-        //método construtor das questões 
-        public function __construct($enunciado,$alternativaA,$alternativaB,$alternativaC,$alternativaD,$questaocerta){
-            $this->enunciado=$enunciado;
-            // declara o array questões, separando as alternativas 4 
-            $this->questões=array(1=> $alternativaA,2=> $alternativaB, 3=> $alternativaC,4=>$alternativaD,);
-            $this->questaocerta=$questaocerta;
-        }
-
-        // Função que imprime as questões
-        public function MostraQuestões($numero){
-        echo"<br> questão: ".$numero."<br>";
-            echo ($this->enunciado."<br>");
-            //armazena cada posição de questões na variavel questão
-            foreach($this->questões as $questao){
-                echo $questao."<br>";
-            }
-            //imprime a alternativa certa 
-            echo ("a alternativa certa é a:" .$this->questaocerta."<br>");  
-         } 
-    }
-    //carrega a função menu 
-    require 'menu.inc';
-
-    //cria a variavel pergunta e chama para ela a função carregaPergunta (presente em perguntas.inc) essa função retorna uma pergunta de acordo com o id 
-    $pergunta=carregaPergunta($id);
-
-$total_perguntas = count($perguntas);
-$id = isset($_GET['id']) ? $_GET['id'] : 1;
-
-if ($id <= $total_perguntas) {
-    $pergunta_atual = $perguntas[$id];
-    
-    echo '<h1>Pergunta ' . $id . '</h1>';
-    echo '<p>' . $pergunta_atual['pergunta'] . '</p>';
-    
-    // Verifica se foi submetida uma resposta
-    if (isset($_POST['resposta'])) {
-        $resposta = $_POST['resposta'];
-        
-        // Verifica se a resposta está correta
-        if ($resposta == $pergunta_atual['resposta_correta']) {
-            echo '<p>Resposta correta!</p>';
-            
-            // Incrementa o ID para carregar a próxima pergunta
-            $proximo_id = $id + 1;
-            if ($proximo_id <= $total_perguntas) {
-                echo '<a href="?id=' . $proximo_id . '">Próxima pergunta</a>';
-            } else {
-                echo '<p>Parabéns! Você respondeu todas as perguntas.</p>';
-            }
-        } else {
-            echo '<p>Resposta incorreta!</p>';
-        }
-    } else {
-        // Formulário para submeter a resposta
-        echo '<form method="POST" action="">';
-        echo '<input type="text" name="resposta" placeholder="Digite sua resposta">';
-        echo '<input type="submit" value="Enviar">';
-        echo '</form>';
-    }
-} else {
-    echo '<p>Pergunta inválida!</p>';
-}
+    <input type="submit" value="Enviar " >
+    </form>
+       <?php endif; ?>
+   
+       <script>
+           function proximaPergunta(id) {
+               id = id + 1;
+               window.location.href = 'perguntas.php?id=' + id;
+           }
+       </script>
+   </body>
+   </html>
+   <?php
     //iprime as perguntas
-    if ($pergunta) {
-        $pergunta->MostraQuestões($id);
-    } else {
-        echo "Pergunta não encontrada.";
-    }
+   // if ($pergunta) {
+     //   $pergunta->MostraQuestões($id);
+  //  } else {
+       // echo "Pergunta não encontrada.";
+   // }
 
     //chama o arquivo, mas se ele não existir o código roda independentemente
     include 'rodape.inc';
