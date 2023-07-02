a3dc0361c30444ac733cc2e09b810133abb73aa1
Inaue <100319268+Inaue@users.noreply.github.com>
2023-07-02 14:08:04 +0000

    Pagina principal do site.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	</head>
+	<body>
+		<?php include 'menu.inc'; ?>
+		<?php include 'imprima_pergunta.inc'; ?>
+		<?php include 'rodape.inc'; ?>
+	</body>
+</html>
