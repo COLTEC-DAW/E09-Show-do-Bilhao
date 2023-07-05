8630c91104cfce01465228587402f21dba5dedde
EduardoBirchal <edubirchal@gmail.com>
2023-07-05 14:34:26 -0300

    ADD: Salvar progresso usando cookies

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
     <body>
         <?php
             
+            require('usuario.inc');
             require('pergunta.inc.php');
             require('definirPerguntas.inc.php');
             require('paginaResultado.inc.php');
-            require('usuario.inc');
 
             include('header.inc');
 
@@ -25,6 +25,11 @@
                 global $idPagina;
 
                 if ($perguntas[$idPagina]->checarResposta()) {
+                    $usuarioAtual = $_SESSION['loginAtual'];
+
+                    // $usuarioAtual->atualizaPontuacaoMaxima($idPagina+1); 
+                    atualizaPontuacaoMaxima($usuarioAtual, $idPagina+1);  
+                    atualizaCookieUsuario();
                     respostaCorreta($idPagina);
                 }
                 else {
