8f75471544689ecc61e60649f1fdf9cb64259b8f
Nicollauda <104602677+Nicolle-Oliveira@users.noreply.github.com>
2023-07-05 14:53:42 -0300

    Agora com cookies!

---
+++ Testes/LoginTestes2/area_principal.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+
+// Verifica se o usuário está logado
+if (!isset($_SESSION["usuario"])) {
+    // Redireciona para a página de login
+    header("Location: index.php");
+    exit;
+}
+
+// Obtém as informações do usuário
+$usuario = $_SESSION["usuario"];
+$usuarios = $_SESSION["TodosUsuarios"];
+
+// Define a hora da última sessão apenas quando o usuário fizer logout
+if (isset($_POST["logout"])) {
+    foreach ($usuarios as $key => $value) {
+        if ($value == $usuario) {
+            unset($usuarios[$key]);
+            break;
+        }
+    }
+
+    $sessao = date("d/m/Y H:i");
+    $usuarios[] = $usuario;
+    file_put_contents("usuarios.json", json_encode($usuarios));
+
+    // Armazena a última sessão em um cookie
+    setcookie("lastSession", $sessao);
+
+    // Encerra a sessão do usuário  
+    session_unset();
+    session_destroy();
+
+    // Redireciona para a página de login
+    header("Location: index.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Área Principal</title>
+</head>
+<body>
+    <h1>Bem-vindo, <?php echo $usuario["username"]; ?>!</h1>
+    <p>Email: <?php echo $usuario["email"]; ?></p>
+    <p>Username: <?php echo $usuario["username"]; ?></p>
+    <p>Última sessão em: <?php echo $_COOKIE["lastSession"] ?></p>
+
+    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+        <input type="submit" name="logout" value="Logout">
+    </form>
+</body>
+</html>
