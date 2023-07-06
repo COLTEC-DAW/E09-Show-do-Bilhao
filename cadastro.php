5502761bb07f4e47921d1afbab46271ce26dc38d
Alisson Horta <alissonhorta1@gmail.com>
2023-07-06 09:14:41 -0300

    vaii

---
+++ cadastro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <form action="perguntas.php">
+        Nome <input type="text" name="nome"><br>
+        Email <input type="email" name="email"><br>
+        Login <input type="text" name="login"><br>
+        Senha <input type="password" name="senha"><br>
+        <input type="submit" name="Log-in">
+    </form>
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-
-    </head> 
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+    </head>
     <body>
-        <form action="teste.php" method="post">
-            Login: <input type="text" name="login"> <br>
-            Senha: <input type="password" name="senha"> <br>
-            <input type="submit" name="Enviar"> 
+        <?php 
+            if (isset($_GET["$id"])) {
+                if ($_GET["$id"] == 1) {
+                    echo "<h3>Login ou senha incorretos</h3>";
+                } 
+            }
+            else {
+                echo "<h3>Bem vindo ao Show do Bilhão</h3>";
+                echo "<h3>Escolha uma das opições abaixo: </h3>";
+            }
+        ?>
+
+        <form action="autenticacao.php" method="post">
+            <button type="submit">Login</button>
+        </form>
+        <form action="cadastro.php" method="post">
+            <button type="submit">Cadastrar</button>
         </form>
     </body>
 </html>
