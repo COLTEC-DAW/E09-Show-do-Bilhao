0be8c013e056566ca4266d2db6fc8edd2679cbf6
Igor Barroso Almeida <igorba867@gmail.com>
2023-07-05 22:18:55 -0300

    up

--- login.php
+++ login.php
@@ -1,15 +1,18 @@
-<?php
-    session_start();
-?>
-
 <h1>Login</h1>
 
 <form action="index.php" method="POST">
-    <label for="nome">Nome: </label>
-    <input type="text">
+    <label for="nome">Login: </label>
+    <input type="text" name="login">
 
     <label for="nome">Senha: </label>
-    <input type="password">
+    <input type="password" name="senha">
+
+    <label for="nome">Nome: </label>
+    <input type="text" name="user">
+
+    <label for="nome">Email: </label>
+    <input type="text" name="email">
+
 
     <input type="submit">
 </form>
