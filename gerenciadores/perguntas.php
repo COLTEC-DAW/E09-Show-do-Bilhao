74913402d5850bc44dd4d4d1b51346190cded83b
lorenaricoy <lricoymartins@gmail.com>
2023-07-07 11:06:23 -0300

    up

--- gerenciadores/perguntas.php
+++ gerenciadores/perguntas.php
@@ -9,15 +9,13 @@ setcookie("ultimo-jogo", date("d/m/Y H:i:s"));
 $resp_usuario = $_POST["pergunta"];
 $verificar= verificaPergunta($id, $resp_usuario);
 
-if($verificar == true && $id<=4 || $id==0){
-    include "./inc/interface/form.inc";
-    if($id!=0 || $verificar == true){
-        setcookie("pontuacao", $_COOKIE["pontuacao"]+1);
-    }
-} elseif($verificar == true && $id>4){ 
+if($verificar == true && $id>4){ 
     setcookie("pontuacao");           
     include "./inc/result/vencedor.inc";
-} else{
+}elseif($verificar == true || $id==0){
+    include "./inc/interface/form.inc";
+    setcookie("pontuacao", $_COOKIE["pontuacao"]+1);
+}else{
     setcookie("pontuacao"); 
     include "./inc/result/gameover.inc";
 }
