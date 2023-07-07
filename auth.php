d26fb4687d641c26debf1ed6eb6a296e03b2186b
Inaue <100319268+Inaue@users.noreply.github.com>
2023-07-07 12:40:47 +0000

    Criando Login.

---
+++ auth.php
@@ -0,0 +1,27 @@
+<?php include 'init.inc' ?>
+<?php include 'menu.inc'; ?>
+<?php include 'classe_usuario.inc'; ?>
+
+<?php
+
+//  EXEMPLO
+
+$t0         = new Gerenciador_de_Usuario();
+$user       = $_POST['user'];
+$senha      = $_POST['passwd'];
+
+if($user == $t0->obter_login() && $t0->senha_correta($senha))
+{
+    $_SESSION['autenticado']    = true;
+    $_SESSION['user']           = $t0;
+    echo "Login realizado com sucesso!<br>\n";
+    echo "Seja bem-vindo " . $t0->obter_nome() . "!<br>\n";
+}
+else
+    echo "Falha na autenticacao! Tente novamente.<br>\n";
+
+echo "<a href='index.php'>Pagina Inicial</a><br>\n";
+
+?>
+
+<?php include 'rodape.inc'; ?>
---
+++ login.php
@@ -0,0 +1,5 @@
+<?php include 'init.inc' ?>
+<?php include 'menu.inc'; ?>
+<?php include 'forms_login.inc'; ?>
+<p>Não tem conta? <a href='cadastro.php'>Crie a sua agora.</a></p>
+<?php include 'rodape.inc'; ?>
