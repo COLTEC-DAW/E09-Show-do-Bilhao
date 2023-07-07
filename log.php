76156d04da25c0181d1f87ac97b38d51af083391
Inaue <100319268+Inaue@users.noreply.github.com>
2023-07-07 13:40:44 +0000

    Renomeando arquivo 'login' -> 'log'

---
+++ log.php
@@ -0,0 +1,12 @@
+<?php include 'init.inc' ?>
+<?php include 'menu.inc'; ?>
+<?php
+
+if($_SESSION['autenticado'])
+    include 'confirmacao_logout.inc';
+else
+    include 'forms_login.inc';
+
+?>
+
+<?php include 'rodape.inc'; ?>
--- login.php
+++
@@ -1,5 +0,0 @@
-<?php include 'init.inc' ?>
-<?php include 'menu.inc'; ?>
-<?php include 'forms_login.inc'; ?>
-<p>Não tem conta? <a href='cadastro.php'>Crie a sua agora.</a></p>
-<?php include 'rodape.inc'; ?>
