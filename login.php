3922c532480151e1a7b409ab2bf5fe291318a6f5
ArthurMacielSs <104994335+ArthurMacielSs@users.noreply.github.com>
2023-07-06 22:49:30 -0300

    comentando o arquivo login

--- login.php
+++ login.php
@@ -1,36 +1,36 @@
 <?php
 session_start();
 
-// Verifica se o usuário já está autenticado, redireciona para a página de perguntas
+// Verifica se o usuário já existe, e se existir manda pra página de perguntas 
 if (isset($_SESSION['usuario'])) {
     header("location: perguntas.php");
     exit();
 }
 
-// Função para verificar as credenciais do usuário
+// Função para verificar as credenciais do usuáriao, recebe o usuario e a senha digitados pelo jogador, e verficica pelo arquivo
 function verificarCredenciais($usuario, $senha) {
-    $usuarios = json_decode(file_get_contents('usuarios.json'), true)['usuarios'];
+    $usuarios = json_decode(file_get_contents('usuarios.json'), true)['usuarios'];//file get contents lê conteudo do arquvio, json_decode(...,true )decodifica uma função json em php, true indica que o resulta é um array e não um objeto [usuarios]indica que pegamos a chave usuarios do array, percorre um vetor só nela (usuarios ta antes dos atributos)
     foreach ($usuarios as $usuarioArmazenado) {
         if ($usuario === $usuarioArmazenado['usuario'] && $senha === $usuarioArmazenado['senha']) {
-            return true; // Credenciais corretas
+            return true; //deu certo 
         }
     }
-    return false; // Credenciais incorretas
+    return false; 
 }
 
 
-
+//verifica se o metodo é post, e se existe um usuario e senha, se for true recolhe o valor de usuario e senha e armazena nas variaveis com o mesmo nome
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['usuario']) && isset($_POST['senha'])) {
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
 
-    // Verifica as credenciais do usuário
+   //se as credenciais forem válidas (true), inicia a sessão com  o nome do usuario e manda para perguntas.php, saindo do resto do código
     if (verificarCredenciais($usuario, $senha)) {
-        $_SESSION['usuario'] = $usuario;
+        $_SESSION['usuario'] = $usuario; 
         header("location: perguntas.php");
         exit();
     } else {
-        $erroLogin = true; // Credenciais incorretas, exibe mensagem de erro
+        $erroLogin = true;  //seta a var de erro 
     }
 }
 ?>
@@ -42,9 +42,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['usuario']) && isset($
 </head>
 <body>
     <h1>Login</h1>
+    <!-- se erroLogin for true -->
     <?php if (isset($erroLogin) && $erroLogin): ?>
         <p>Usuário ou senha incorretos.</p>
     <?php endif; ?>
+    <!-- define um formulario, que vai ser post. sua ação (para onde é enviado), e um função que retorna o endereço do proprio arquivo, depois cria um label (descrição) para usuario e senha for representa a quem ele tá representado, que é o id do campo, type quer dizer o texto, name o nome (usado para o post) e id é para o label, required quer dizer que precisa preencher para enviar-->
     <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <label for="usuario">Usuário:</label>
         <input type="text" name="usuario" id="usuario" required>
