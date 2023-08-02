1a91b7dd2125321b4683d64d9e9656f6d83f3553
Inaue <100319268+Inaue@users.noreply.github.com>
2023-08-02 13:31:57 +0000

    Cookies de ultimo login.

--- logout_realizado.php
+++ logout_realizado.php
@@ -1,11 +1,14 @@
 <?php include 'init.inc'; ?>
-<?php include 'menu.inc'; ?>
-
 <?php
 
+setcookie('tempo_ultimo_login_' . $_SESSION['user']->obter_login(), date("H:i:s \d\o \d\i\a d/m/Y"));
+
+?>
+<?php include 'menu.inc'; ?>
+<?php
 session_unset();
 session_destroy();
 
 ?>
-
 <p>Saida efetuado com exito!</p>
+<?php include 'rodape.inc'; ?>
