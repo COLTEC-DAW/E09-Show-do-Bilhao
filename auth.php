59f9890c1903ad43c0fe423340d8f9dda57807d0
Inaue <100319268+Inaue@users.noreply.github.com>
2023-08-02 13:29:56 +0000

    Criando autenticação do usuário.

--- auth.php
+++ auth.php
@@ -1,23 +1,41 @@
-<?php include 'init.inc' ?>
+<?php include 'init.inc'; ?>
 <?php include 'menu.inc'; ?>
 
 <?php
 
 //  EXEMPLO
 
-$t0         = new Gerenciador_de_Usuario();
-$user       = $_POST['user'];
+$usuario    = new Gerenciador_de_Usuario();
+$usuarios   = fopen('users.json', 'r');
+$user_name  = $_POST['user'];
 $senha      = $_POST['passwd'];
 
-if($user == $t0->obter_login() && $t0->senha_correta($senha))
+if($usuarios != null)
 {
-    $_SESSION['autenticado']    = true;
-    $_SESSION['user']           = $t0;
-    echo "Login realizado com sucesso!<br>\n";
-    echo "Seja bem-vindo " . $t0->obter_nome() . "!<br>\n";
+    while(!$_SESSION['autenticado'])
+    {
+        $usuario->setFromJSON(fgets($usuarios));
+        
+        if(feof($usuarios))
+            break;
+
+        if($user_name == $usuario->obter_login() && $usuario->senha_correta($senha))
+        {
+            $_SESSION['autenticado']    = true;
+            $_SESSION['user']           = $usuario;
+            echo "Login realizado com sucesso!<br>\n";
+            echo "Seja bem-vindo " . $usuario->obter_nome() . "!<br>\n";
+        
+            if(isset($_COOKIE['tempo_ultimo_login_' . $_SESSION['user']->obter_login()]))
+                echo "Você jogou pela ultima vez em " . $_COOKIE['tempo_ultimo_login_' . $_SESSION['user']->obter_login()] . "<br>\n";
+        }
+    }
 }
-else
-    echo "Falha na autenticacao! Tente novamente.<br>\n";
+
+fclose($usuarios);
+
+if(!$_SESSION['autenticado'])
+    echo "Falha na autenticacao! Tente novamente.<br>\n";    
 
 ?>
 
