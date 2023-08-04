125cd73ef0c22b623e39af236f93e92de338e113
Inaue <100319268+Inaue@users.noreply.github.com>
2023-08-04 14:13:27 +0000

    Criando página de perguntas.

--- pergunta.php
+++ pergunta.php
@@ -1,4 +1,15 @@
 <?php include 'init.inc'; ?>
+<?php
+
+if($_COOKIE['errou_' . $_SESSION['user']->obter_login()] == "true")
+{
+    include 'pagina_erro.inc';
+}
+else
+{
+    include 'forms_pergunta.inc';
+}
+
+?>
 <?php include 'menu.inc'; ?>
-<?php include 'forms_pergunta.inc'; ?>
 <?php include 'rodape.inc'; ?>
