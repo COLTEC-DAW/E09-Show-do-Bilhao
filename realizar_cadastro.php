a03a827b2d1d5b90d927e9f824b969a1d41aa01c
Inaue <100319268+Inaue@users.noreply.github.com>
2023-08-04 14:10:53 +0000

    Corrigindo realização de cadastro.

--- realizar_cadastro.php
+++ realizar_cadastro.php
@@ -1,5 +1,4 @@
 <?php include 'init.inc'; ?>
-<?php include 'menu.inc'; ?>
 <?php
 
 $novo_user  = new Gerenciador_de_Usuario($_POST['name'], $_POST['email'], $_POST['user'], $_POST['passwd']);
@@ -7,9 +6,10 @@ $usuarios   = fopen('users.txt', 'a');
 fwrite($usuarios, $novo_user->toJSON() . "\n");
 fclose($usuarios);
 
-setcookie('pontuacao_' . $_POST['user'], 0);
-setcookie('errou_' . $_POST['user'], false);
+setcookie('pontuacao_' . $novo_user->obter_login(), 0);
+setcookie('errou_' . $novo_user->obter_login(), "false");
 
 ?>
 <p>Conta criada com sucesso!</p>
+<?php include 'menu.inc'; ?>
 <?php include 'rodape.inc'; ?>
